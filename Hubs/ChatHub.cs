de926d71db69d02a866c269731a32db2490cf385
ddgupta <ddgupta@appzero.com>
2016-12-05 12:01:10 -0500

    Initial commit.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Test.Hubs
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void Whassup()
+        {
+            Clients.All.sayWhassup($"{Context.ConnectionId} says: <h1>Whassup</h1><hr>");
+        }
+
+    }
+}
